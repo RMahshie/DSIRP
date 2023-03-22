0505b60c693ef6bff2d17ee15f1202de2ea457a0
Ryan Mahshie <ryan@mahshie.net>
2023-03-22 12:37:42 -0400

    working on insert function

---
+++ notebooks/OOP.py
@@ -0,0 +1,6 @@
+class Employee(object):
+   def my_method(self):
+      print("I am Vikram")
+name = Employee("ryan")
+name.my_method()
+print(type(name))
--- notebooks/PQ.py
+++ notebooks/PQ.py
@@ -16,16 +16,44 @@ class PQ:
 
 def new():
         
-    '''
+    """
     >>> pq = new()
     >>> isinstance(pq, PQ)
     True
-    '''
+    """
     return PQ()
 
+def insert(val, pq):
+    
+    """
+    >>> pq = new()
+    >>> pq.size()
+    0
+    >>> insert(7, pq)
+    >>> pq.size()
+    1
+    >>> insert(6, pq)
+    >>> pq.size()
+    2
+    """
+    pq.data.append(val)
 
 
 
+def printElements(pq):
+    '''
+    
+    >>> pq = new()
+    >>> insert(7, pq)
+    
+    >>> pq.size()
+    
+    '''
+    print(pq.data)
+
+pq = new()
+insert(7, pq)
+printElements(pq)
 
 
 if __name__ == "__main__":
