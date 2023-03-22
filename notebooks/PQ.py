d6e74f9e715901727275b89244a180dec3f5af10
Ryan Mahshie <ryan@mahshie.net>
2023-03-22 12:50:03 -0400

    insert function passes test now

--- notebooks/PQ.py
+++ notebooks/PQ.py
@@ -29,27 +29,21 @@ def insert(pq, value, Append):
     >>> pq = new()
     >>> pq.size()
     0
-    >>> insert(7, pq)
-    >>> pq.size()
-    1
-    >>> insert(6, pq)
-    >>> pq.size()
-    2
+    >>> insert(pq, 7, True)
+    [7]
+    >>> insert(pq, 6, True)
+    [6, 7]
     """
    
     storage = 0
     
     if Append:
-        print("before adding value ", pq.data)
         pq.data.append(value)
-        print("after adding value: ", pq.data)
     
     valueI = pq.data.index(value)
     parent = pq.data[(pq.data.index(value)-1)//2]
     parentI = (pq.data.index(value)-1)//2
     
-    print("value: ", value)
-    print("parent: ", parent)
     if pq.data.index(value) == 0:
         return pq.data
     
@@ -57,16 +51,11 @@ def insert(pq, value, Append):
         
         pq.data[parentI], pq.data[valueI] =  pq.data[valueI], pq.data[parentI]
         
-        print("after swap: ", pq.data)
         insert(pq, value, False)
     
-    return 'Sorted Heap: ', pq.data
+    return pq.data
 
-pq = new()
-insert(pq, 7, True)
-insert(pq, 8, True)
-insert(pq, 5, True)
-insert(pq, 1, True)
+
 
 
 if __name__ == "__main__":
