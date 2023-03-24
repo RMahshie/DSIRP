88e771ae17ee0fc9006bfc52420cc8ecd65ebeef
Ryan Mahshie <ryan@mahshie.net>
2023-03-23 22:02:29 -0400

    trying to fix dm function

--- notebooks/OOP.py
+++ notebooks/OOP.py
@@ -1,6 +1,8 @@
 class Employee(object):
    def my_method(self):
       print("I am Vikram")
-name = Employee("ryan")
+name = Employee()
 name.my_method()
 print(type(name))
+
+ 
--- notebooks/PQ.py
+++ notebooks/PQ.py
@@ -56,8 +56,50 @@ def insert(pq, value, Append):
     return pq.data
 
 
+def dm(pq, value, Assign):
+    if Assign:
+        print("pq.data: ", pq.data)
+        print("value: ", value)
+        pq.data[0] = value
+        print("pq.data after setting first to value: ", pq.data)
+        pq.data.pop()
+    leftParent = (pq.data.index(value)*2)+1
+    rightParent = (pq.data.index(value)*2)+2
+    print("pq.data: ", pq.data)
+    print("value index: ",pq.data.index(value))
+    if (leftParent) < len(pq.data):
+        p1 = pq.data[(pq.data.index(value)*2)+1]
+        p1Index = (pq.data.index(value)*2)+1
+    if ((pq.data.index(value)*2)+2) < len(pq.data):
+        p2 = pq.data[(pq.data.index(value)*2)+2]
+        p2Index = (pq.data.index(value)*2)+2
+    else:
+        return pq.data
+    print("left parent", p1)
+    print("right parent", p2)
+    print("Swap Left?", p1<p2)
+    left = p1<p2
+    print()
+    if value <= p1:
+        return pq.data
+    elif left and (p1Index) < len(pq.data) :
+            pq.data[p1Index], pq.data[pq.data.index(value)] = pq.data[pq.data.index(value)], pq.data[p1Index]
+            dm(pq, value, False)
+    elif not left and (p2Index) < len(pq.data):
+        pq.data[p2Index], pq.data[pq.data.index(value)] = pq.data[pq.data.index(value)], pq.data[p2Index]
+        dm(pq, value, False)
+    else:
+        return pq.data
 
+    
 
+pq = new()
+insert(pq, 7, True)
+insert(pq, 6, True)
+insert(pq, 5, True)
+insert(pq, 4, True)
+dm(pq, pq.data[len(pq.data)-1], True)
+print(pq.data)
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
