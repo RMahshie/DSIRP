454e67f0bdfa30b6309642bab78ffef98934399d
Ryan Mahshie <ryan@mahshie.net>
2023-04-13 11:41:25 -0400

    stuff

---
+++ myWork/ptwice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void print_twice(char phil) {
+    cout << phil << phil << endl;
+}
+
+int main() {
+    char argument = "boobookeys";
+    print_twice(argument);
+    return 0;
+}
